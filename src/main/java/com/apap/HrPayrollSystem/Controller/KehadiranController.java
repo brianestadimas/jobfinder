6953dc062c66aad6635d1dd179ffbdd883bfe1e0
aji wuryanto <ajiw4056@gmail.com>
2019-04-01 22:40:09 +0700

    progress detail per kehadiran

--- src/main/java/com/apap/HrPayrollSystem/Controller/KehadiranController.java
+++ src/main/java/com/apap/HrPayrollSystem/Controller/KehadiranController.java
@@ -21,14 +21,14 @@ public class KehadiranController {
 	@Autowired
 	private PegawaiOutsourcingService pegawai_outsourcing_service;
 	
-	@RequestMapping(value="/proyek/{proyekId}/kehadiran",method=RequestMethod.GET)
-	private String daftarKehadiranProyek(@PathVariable(value="proyekId") long proyekId, Model model) {
+	@RequestMapping(value="/proyek/{proyek_id}/kehadiran",method=RequestMethod.GET)
+	private String daftarKehadiranProyek(@PathVariable(value="proyek_id") long proyek_id, Model model) {
 		
 		List<KehadiranModel> get_all_kehadiran = kehadiran_service.get_all_kehadiran();
 		List<String> kehadiran_proyek_ini = new ArrayList<String>();
 		
 		for(int i = 0 ; i < get_all_kehadiran.size() ; i++) {
-			if(get_all_kehadiran.get(i).getProyek().getId() == proyekId) {
+			if(get_all_kehadiran.get(i).getProyek().getId() == proyek_id) {
 				if(kehadiran_proyek_ini.isEmpty()) {
 					kehadiran_proyek_ini.add(get_all_kehadiran.get(i).getJudul_kehadiran());
 				}
@@ -46,9 +46,31 @@ public class KehadiranController {
 		return "list_kehadiran";
 	}
 	
+
+	//Lihat Detail Kehadiran
+	@RequestMapping(value="/proyek/{proyek_id}/kehadiran/{judul_kehadiran}", method=RequestMethod.GET)
+	private String detailKehadiranProyek(@PathVariable(value="proyek_id") long proyek_id,
+										@PathVariable(value="judul_kehadiran") String judul_kehadiran,
+										Model model) {
+		
+		List<KehadiranModel> get_all_kehadiran = kehadiran_service.get_all_kehadiran();
+		List<KehadiranModel> detail_kehadiran = new ArrayList<KehadiranModel>();
+		for(int i = 0 ; i < get_all_kehadiran.size() ; i++) {
+			if(get_all_kehadiran.get(i).getProyek().getId() == proyek_id) {
+				if(get_all_kehadiran.get(i).getJudul_kehadiran() == judul_kehadiran) {
+					detail_kehadiran.add(get_all_kehadiran.get(i));
+				}
+			}
+		}
+		model.addAttribute("detail_detail_kehadiran", detail_kehadiran);
+		return "detail_kehadiran";
+	}
 	//ADD KEHADIRAN
 	//iterasi semua pegawai yg ada di proyek itu NIP dan Nama Pegawainya
 	//sebelahnya form kosong hadir, sakit, izin, alfa, libur, cuti, lain-lain
 	
+	//Update Kehadiran
+	
+	//Hapus Kehadiran
 	
 }
