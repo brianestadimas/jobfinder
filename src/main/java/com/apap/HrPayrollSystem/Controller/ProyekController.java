fe0825747c9dddc7902ee8567b129694b8ab2256
brians.estadimas <--brians.estadimas@gmail.com>
2019-04-04 11:54:55 +0700

    50% backend

---
+++ src/main/java/com/apap/HrPayrollSystem/Controller/ProyekController.java
@@ -0,0 +1,75 @@
+package com.apap.HrPayrollSystem.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.apap.HrPayrollSystem.Model.ProyekModel;
+import com.apap.HrPayrollSystem.Service.PegawaiOutsourcingService;
+import com.apap.HrPayrollSystem.Service.ProyekService;
+
+@Controller
+public class ProyekController {
+	@Autowired
+	private ProyekService proyekService;
+	@Autowired
+	private PegawaiOutsourcingService pegawaiService;
+	
+	@RequestMapping("/proyek")
+	private String proyek(Model model) {
+		List<ProyekModel> list = proyekService.getAllProyek();
+		model.addAttribute("listProyek", list);
+		
+		return "list_proyek";
+	}
+	
+	@RequestMapping(value = "/proyek-detail/{id}", method = RequestMethod.GET)
+	private String detailProyek(@PathVariable long id, Model model) {
+		ProyekModel proyek = proyekService.getProyekById(id).get();
+		model.addAttribute("proyek", proyek);
+		
+		if (proyek.getJenis_proyek()=="umum") {
+			return "detail_proyek_umum";
+		}
+		return "detail_proyek_unik";
+	}
+	
+	//Delete Proyek
+	
+	@RequestMapping(value = "/proyek-hapus", method = RequestMethod.POST)
+	private String deleteProyek(@RequestParam("id") long id, Model model) {
+		
+		try {
+			proyekService.deleteById(id);
+			return "list_proyek";
+		} catch(Exception e) {
+			return null;
+		}
+		
+	}
+	
+	//Add Proyek
+	
+	@RequestMapping(value = "/proyek-tambah", method = RequestMethod.GET)
+	public String addJabatanGET(Model model) {
+		ProyekModel proyek = new ProyekModel();
+		model.addAttribute("proyek", proyek);
+		
+		return "tambah_proyek";
+	}
+	
+	@RequestMapping(value = "/proyek-tambah", method = RequestMethod.POST)
+	public String addJabatan(Model model, @ModelAttribute ProyekModel proyek) {
+		proyekService.addProyek(proyek);
+		
+		return "list_proyek";
+	}
+	
+}
--- src/main/java/com/apap/HrPayrollSystem/Model/ProyekModel.java
+++ src/main/java/com/apap/HrPayrollSystem/Model/ProyekModel.java
@@ -42,6 +42,10 @@ public class ProyekModel implements Serializable{
 	private String nama_cp;
 	
 	@Size(max=255)
+	@Column(name="deskripsi_proyek",nullable = true)
+	private String deskripsi_proyek;
+	
+	@Size(max=255)
 	@Column(name="no_telp_cp",nullable = true)
 	private String no_telp_cp;
 	
@@ -62,7 +66,7 @@ public class ProyekModel implements Serializable{
 	@NotNull
 	@Size(max=255)
 	@Column(name="jenis_proyek",nullable = false)
-	private long jenis_proyek;
+	private String jenis_proyek;
 	
 	@Size(max=255)
 	@Column(name="biaya_keseluruhan",nullable = true)
@@ -164,11 +168,11 @@ public class ProyekModel implements Serializable{
 		this.no_rekening = no_rekening;
 	}
 
-	public long getJenis_proyek() {
+	public String getJenis_proyek() {
 		return jenis_proyek;
 	}
 
-	public void setJenis_proyek(long jenis_proyek) {
+	public void setJenis_proyek(String jenis_proyek) {
 		this.jenis_proyek = jenis_proyek;
 	}
 
