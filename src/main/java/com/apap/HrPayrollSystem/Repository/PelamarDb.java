91e2c3e0df8187632603faf9161fce2112cc45d2
brians.estadimas <brians.estadimas@gmail.com>
2019-06-14 06:22:06 +0700

    final

---
+++ src/main/java/com/apap/HrPayrollSystem/Repository/PelamarDb.java
@@ -0,0 +1,13 @@
+package com.apap.HrPayrollSystem.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apap.HrPayrollSystem.Model.PelamarModel;
+
+@Repository
+public interface PelamarDb extends JpaRepository<PelamarModel,Long>{
+	
+	PelamarModel findById(long id);
+
+}
