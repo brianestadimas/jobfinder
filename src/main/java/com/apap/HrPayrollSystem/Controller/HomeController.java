ec0a459000b1f4c0441528d899e65d7893439f1f
brians.estadimas <brians.estadimas@gmail.com>
2019-12-10 08:17:12 +0700

    maintenance system, remove table home

--- src/main/java/com/apap/HrPayrollSystem/Controller/HomeController.java
+++ src/main/java/com/apap/HrPayrollSystem/Controller/HomeController.java
@@ -39,20 +39,20 @@ public class HomeController {
 		List<ProyekModel> list_of_proyek = new ArrayList<ProyekModel>();
 		List<PelamarModel> pelamar_belum_assign = new ArrayList<PelamarModel>();
 		List<PelamarModel> pelamar_belum_assign_full = new ArrayList<PelamarModel>();
-		for(int i = 0 ; i < pelamar_service.getAllPelamar().size() ; i++) {
-			if(pelamar_service.getAllPelamar().get(i).isIs_pegawai()==false) {
-				if(pelamar_belum_assign.size()<7) {
-					pelamar_belum_assign.add(pelamar_service.getAllPelamar().get(i));
-				}
-				pelamar_belum_assign_full.add(pelamar_service.getAllPelamar().get(i));
+		// for(int i = 0 ; i < pelamar_service.getAllPelamar().size() ; i++) {
+		// 	if(pelamar_service.getAllPelamar().get(i).isIs_pegawai()==false) {
+		// 		if(pelamar_belum_assign.size()<7) {
+		// 			pelamar_belum_assign.add(pelamar_service.getAllPelamar().get(i));
+		// 		}
+		// 		pelamar_belum_assign_full.add(pelamar_service.getAllPelamar().get(i));
 				
-			}
-		}
-		for(int i = 0 ; i < proyek_service.getAllProyek().size() ; i++) {
-			if(list_of_proyek.size()<7) {
-				list_of_proyek.add(proyek_service.getAllProyek().get(i));
-			}
-		}
+		// 	}
+		// }
+		// for(int i = 0 ; i < proyek_service.getAllProyek().size() ; i++) {
+		// 	if(list_of_proyek.size()<7) {
+		// 		list_of_proyek.add(proyek_service.getAllProyek().get(i));
+		// 	}
+		// }
 		model.addAttribute("user", user);
 		model.addAttribute("pelamar", pelamar_belum_assign);
 		model.addAttribute("proyek",list_of_proyek);
