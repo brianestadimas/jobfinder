f0df3b204cf7e8a46993acd79a4e9397a59db277
brians.estadimas <brians.estadimas@gmail.com>
2020-04-28 10:39:27 +0700

    perbaikan

--- src/main/java/com/apap/HrPayrollSystem/Repository/PelamarDb.java
+++ src/main/java/com/apap/HrPayrollSystem/Repository/PelamarDb.java
@@ -1,5 +1,7 @@
 package com.apap.HrPayrollSystem.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
