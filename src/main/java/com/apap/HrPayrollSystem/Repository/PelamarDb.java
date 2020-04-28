6a170c68a665f8918208ebfd9d86f22131883243
brians.estadimas <brians.estadimas@gmail.com>
2020-04-28 10:28:36 +0700

    perbaikan

--- src/main/java/com/apap/HrPayrollSystem/Repository/PelamarDb.java
+++ src/main/java/com/apap/HrPayrollSystem/Repository/PelamarDb.java
@@ -10,8 +10,8 @@ import com.apap.HrPayrollSystem.Model.PelamarModel;
 @Repository
 public interface PelamarDb extends JpaRepository<PelamarModel,Long>{
 
-	@Query(value="SELECT * FROM PelamarModel WHERE is_pegawai = :statusNow", nativeQuery=true) 
-	List<PelamarModel> findAllPelamar(@Param("statusNow") boolean statusNow);
+	@Query(value="SELECT * FROM PelamarModel WHERE is_pegawai = false", nativeQuery=true) 
+	List<PelamarModel> findAllPelamar();
 	
 	PelamarModel findById(long id);
 
